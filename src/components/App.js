365d36e0a7cc1890db0ca4374e6dc712db546431
Brandon Castro <castroyou999@gmail.com>
2023-01-09 10:16:15 -0500

    Initial changes by Student

--- src/components/App.js
+++ src/components/App.js
@@ -1,1 +1,31 @@
 // create your App component here
+import React, {useState, useEffect} from "react";
+
+function App(){
+
+    const [isDog, setDog] = useState(null)
+    // const [isContentLoaded, setContentLoaded] = useState(false)
+
+    useEffect(()=> {
+        fetch("https://dog.ceo/api/breeds/image/random")
+        .then(resp => resp.json())
+        .then(data => {
+            setDog(data.message);
+            // setContentLoaded(true);
+        })
+    }, [])
+
+
+    if (!isDog){
+        return (
+            <p>Loading...</p>
+        )
+    }
+    return (
+        <div>
+            <img src={isDog} alt="A Random Dog" /> 
+        </div>
+    )
+}
+
+export default App
